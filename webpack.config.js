3a6da1cc1b0f61f319aa9c23aded273643bedcb2
luzh <332862394@qq.com>
2020-07-31 16:41:31 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const { title } = require("process");
 
 module.exports = {
   mode: "development",
@@ -6,5 +8,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack测试",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
